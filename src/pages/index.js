84aca7c87f8ca7319aa5fc6f08e734af9ac5d3f3
Thuong To <thuongthanhto@gmail.com>
2023-04-08 13:06:58 +0700

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import {
   Tab,
   TabPanel,
 } from '@material-tailwind/react';
+import Gallary from '@/components/Gallery';
 
 export default function Home() {
   return (
@@ -53,84 +54,14 @@ export default function Home() {
             </TabsHeader>
             <TabsBody>
               <TabPanel value="studio">
-                {' '}
-                <div className="flex justify-around">
-                  <div className="p-10">
-                    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                      <img className="w-full" src="/temp.jpg" alt="Mountain" />
-                      <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2">Mountain</div>
-                        <p className="text-gray-700 text-base">
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Voluptatibus quia, Nonea! Maiores et perferendis
-                          eaque, exercitationem praesentium nihil.
-                        </p>
-                      </div>
-                      <div className="px-6 pt-4 pb-2">
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #photography
-                        </span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #travel
-                        </span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #winter
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="p-10">
-                    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                      <img className="w-full" src="/temp.jpg" alt="Mountain" />
-                      <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2">Mountain</div>
-                        <p className="text-gray-700 text-base">
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Voluptatibus quia, Nonea! Maiores et perferendis
-                          eaque, exercitationem praesentium nihil.
-                        </p>
-                      </div>
-                      <div className="px-6 pt-4 pb-2">
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #photography
-                        </span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #travel
-                        </span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #winter
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="p-10">
-                    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                      <img className="w-full" src="/temp.jpg" alt="Mountain" />
-                      <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2">Mountain</div>
-                        <p className="text-gray-700 text-base">
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Voluptatibus quia, Nonea! Maiores et perferendis
-                          eaque, exercitationem praesentium nihil.
-                        </p>
-                      </div>
-                      <div className="px-6 pt-4 pb-2">
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #photography
-                        </span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #travel
-                        </span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #winter
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <Gallary />
               </TabPanel>
-              <TabPanel value="reportage">Studio</TabPanel>
-              <TabPanel value="family">Studio</TabPanel>
+              <TabPanel value="reportage">
+                <Gallary />
+              </TabPanel>
+              <TabPanel value="family">
+                <Gallary />
+              </TabPanel>
             </TabsBody>
           </Tabs>
         </section>
