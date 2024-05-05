a7a488b456f52b4c2d31fb840b5c52029558d6be
Thuong To <thuongthanhto@gmail.com>
2024-05-05 22:17:42 +0700

    Update contact

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,16 +1,30 @@
 import { IconButton } from '@material-tailwind/react';
-import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import { FaEnvelope, FaFacebook, FaPhone } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className="footer flex flex-col items-center border-t-2 pt-4">
       <div className="flex my-3">
-        <IconButton variant="outlined" className="mx-2">
+        <IconButton
+          variant="outlined"
+          className="mx-2"
+          onClick={() =>
+            window.open('https://www.facebook.com/PhatBo0905', '_blank')
+          }
+        >
           <FaFacebook className="text-[18px]" />
         </IconButton>
 
         <IconButton variant="outlined" className="mx-2">
-          <FaInstagram className="text-[18px]" />
+          <a href="tel:+84909383332">
+            <FaPhone className="text-[18px]" />
+          </a>
+        </IconButton>
+
+        <IconButton variant="outlined" className="mx-2">
+          <a href="mailto:phatchay95@gmail.com">
+            <FaEnvelope className="text-[18px]" />
+          </a>
         </IconButton>
       </div>
       <p className="text-[#222222] mb-[20px]">
